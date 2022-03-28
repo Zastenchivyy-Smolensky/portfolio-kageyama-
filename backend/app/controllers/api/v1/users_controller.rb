058bceaca92c2358a9f99f29fea93aa72421476e
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-03-28 18:53:33 +0900

    User pagegができた.

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UsersController < ApplicationController
     before_action :set_user, only: %i[show update]
     def index
-        users = User.all
-        render json: {status:200, user: users}
+        render json: { users: User.all.order("created_at DESC") }
+
     end
     def show
         render json:{status: 200, user: @user}
